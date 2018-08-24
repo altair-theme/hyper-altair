7db08b489a1c755d5da5dfcdca4e40cec4cb50ae
vsailev <vsailev@users.noreply.github.com>
2018-08-24 14:46:51 +0100

    Indenting Lines

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 'use strict';
-const foregroundColorColor = '#f3f4ef';
-const backgroundColor = '#22242d';
-const red = '#ff1654';
-const green = '#06d6a0';
-const yellow = '#ffe66d';
-const blue = '#4ecdc4';
-const magenta = '#ff60b5';
-const cyan = '#7bdff2';
-const white = '#f9f9f9';
+const foregroundColorColor	= '#f3f4ef';
+const backgroundColor		= '#22242d';
+const red 					= '#ff1654';
+const green 				= '#06d6a0';
+const yellow 				= '#ffe66d';
+const blue 					= '#4ecdc4';
+const magenta 				= '#ff60b5';
+const cyan 					= '#7bdff2';
+const white 				= '#f9f9f9';
 
-exports.decorateConfig = (config) => Object.assign({}, config, {
-	backgroundColor,
-	foregroundColorColor,
-	borderColor: '#211f30',
-	cursorColor: '#b4b4ba',
-	cursorAccentColor: backgroundColor,
-	selectionColor: 'rgba(78, 205, 196, 0.4)',
-	colors: {
-		black: backgroundColor,
-		red,
-		green,
-		yellow,
-		blue,
-		magenta,
-		cyan,
-		white,
-		lightBlack: '#7a7a7a',
-		lightRed: red,
-		lightGreen: green,
-		lightYellow: yellow,
-		lightBlue: blue,
-		lightMagenta: magenta,
-		lightCyan: cyan,
-		lightWhite: foregroundColorColor
-	},
-	css: `
-		/* Custom CSS */
-		${config.css}
-	`
-});
+exports.decorateConfig = (config) => {
+	return Object.assign({}, config, {
+		backgroundColor,
+		foregroundColorColor,
+		borderColor: '#211f30',
+		cursorColor: '#b4b4ba',
+		cursorAccentColor: backgroundColor,
+		selectionColor: 'rgba(78, 205, 196, 0.4)',
+		colors: {
+			black: backgroundColor,
+			red,
+			green,
+			yellow,
+			blue,
+			magenta,
+			cyan,
+			white,
+			lightBlack: '#7a7a7a',
+			lightRed: red,
+			lightGreen: green,
+			lightYellow: yellow,
+			lightBlue: blue,
+			lightMagenta: magenta,
+			lightCyan: cyan,
+			lightWhite: foregroundColorColor
+		},
+		css: `
+			/* Custom CSS */
+			${config.css}
+		`
+	});
+};
